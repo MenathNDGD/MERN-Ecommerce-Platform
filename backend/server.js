33b702c559b0973f3020794f1c1a10cc348d5b24
Menath Nadungoda <nadungmenath1@gmail.com>
2024-08-31 01:39:44 +0530

    added the cookie parser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 
@@ -10,7 +11,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // INFO: Allows to parse the body of the request
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
