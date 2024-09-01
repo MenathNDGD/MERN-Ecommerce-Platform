25940278c91e4731e0de898c4223311443ed62a0
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-01 15:43:42 +0530

    added routes to get profile details

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 import {
   login,
   logout,
   signup,
   refreshToken,
+  getProfile,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
@@ -12,7 +15,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
-// TODO: route get profile
-// router.get("/profile", getProfile);
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
