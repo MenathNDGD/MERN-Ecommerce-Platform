bbcd58c2b5ec66f47a1e2d347b4ef45804e26cd8
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-03 23:35:55 +0530

    modified the home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import CategoryItem from "../components/CategoryItem";
 
 const categories = [
-  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
-  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
-  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
-  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
-  { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
-  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+  { href: "/Jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/T-Shirts", name: "T-Shirts", imageUrl: "/tshirts.jpg" },
+  { href: "/Shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+  { href: "/Glasses", name: "Glasses", imageUrl: "/glasses.png" },
+  { href: "/Jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+  { href: "/Suits", name: "Suits", imageUrl: "/suits.jpg" },
+  { href: "/Bags", name: "Bags", imageUrl: "/bags.jpg" },
 ];
 
 const HomePage = () => {
