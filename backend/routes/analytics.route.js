71c606f577e1e0ec2c4e581e00e9ce0d10ba636a
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-05 22:10:43 +0530

    modified the analytics routes

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import { getAnalyticsData, getDailySalesData } from "../controllers/analytics.controller.js";
+import {
+  getAnalyticsData,
+  getDailySalesData,
+} from "../controllers/analytics.controller.js";
 
 const router = express.Router();
 
@@ -11,11 +14,11 @@ router.get("/", protectRoute, adminRoute, async (req, res) => {
     const endDate = new Date();
     const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
 
-    const dailySalesDate = await getDailySalesData(startDate, endDate);
+    const dailySalesData = await getDailySalesData(startDate, endDate);
 
     res.json({
       analyticsData,
-      dailySalesDate,
+      dailySalesData,
     });
   } catch (error) {
     console.log("Error in analytics route: ", error.message);
